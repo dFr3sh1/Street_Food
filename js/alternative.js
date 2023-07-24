2909d0d0918b1d7e465e6ce8449b281b1cecb281
DFr3sh <dfr3sh1@protonmail.com>
2023-07-24 18:49:47 +0200

    Adding carousel css and responsive

--- js/alternative.js
+++ js/alternative.js
@@ -84,3 +84,92 @@ function autoSlide() {
 }
 
 autoSlide();
+
+
+// ----------------------------//-------------------------------//--------------------------/:
+
+const carouselContainer = document.getElementById("carousel-container");
+const carouselSlides = document.querySelector(".carousel-slides");
+const prevButton = document.getElementById("prev-button");
+const nextButton = document.getElementById("next-button");
+const slidePosition = document.getElementById("slide-position");
+
+let slideIndex = 0;
+const slides = document.querySelectorAll(".menu-accueil");
+const totalSlides = slides.length;
+
+function showSlide(index) {
+  carouselSlides.style.transform = `translateX(-${index * 100}%)`;
+  slidePosition.textContent = `${index + 1} / ${totalSlides}`;
+}
+
+function prevSlide() {
+  slideIndex = (slideIndex - 1 + totalSlides) % totalSlides;
+  showSlide(slideIndex);
+}
+
+function nextSlide() {
+  slideIndex = (slideIndex + 1) % totalSlides;
+  showSlide(slideIndex);
+}
+
+prevButton.addEventListener("click", prevSlide);
+nextButton.addEventListener("click", nextSlide);
+
+// Automatically change slides every 3 seconds
+function autoSlide() {
+  nextSlide();
+  setTimeout(autoSlide, 3000);
+}
+
+autoSlide();
+
+// ------------------------ // ------------------------ //  ----------------------- //
+
+// JavaScript for Mobile Carousel
+document.addEventListener("DOMContentLoaded", function () {
+  const slidesContainer = document.querySelector(".carousel-slides");
+  const slides = document.querySelectorAll(".menu-accueil");
+  const prevButton = document.getElementById("prev-button");
+  const nextButton = document.getElementById("next-button");
+  const slidePosition = document.getElementById("slide-position");
+
+  let currentSlide = 0;
+
+  // Show the carousel controls (buttons and position indicator) on mobile
+  const carouselControls = document.getElementById("carousel-btn");
+  carouselControls.style.display = "block";
+
+  // Update the slide position indicator
+  function updateSlidePosition() {
+    slidePosition.textContent = `${currentSlide + 1} / ${slides.length}`;
+  }
+
+  // Show the current slide and hide the rest
+  function showSlide() {
+    slides.forEach((slide, index) => {
+      slide.style.display = index === currentSlide ? "inline-block" : "none";
+    });
+  }
+
+  // Move to the next slide
+  function nextSlide() {
+    currentSlide = (currentSlide + 1) % slides.length;
+    showSlide();
+    updateSlidePosition();
+  }
+
+  // Move to the previous slide
+  function prevSlide() {
+    currentSlide = (currentSlide - 1 + slides.length) % slides.length;
+    showSlide();
+    updateSlidePosition();
+  }
+
+  // Attach event listeners to buttons
+  nextButton.addEventListener("click", nextSlide);
+  prevButton.addEventListener("click", prevSlide);
+
+  // Start the carousel by moving to the next slide every 3 seconds
+  setInterval(nextSlide, 3000);
+});
--- js/main.js
+++ js/main.js
@@ -1,35 +1,52 @@
-const carouselContainer = document.getElementById("carousel-container");
-const carouselSlides = document.querySelector(".carousel-slides");
-const prevButton = document.getElementById("prev-button");
-const nextButton = document.getElementById("next-button");
-const slidePosition = document.getElementById("slide-position");
+document.addEventListener("DOMContentLoaded", function () {
+  const slidesContainer = document.querySelector(".carousel-slides");
+  const slides = document.querySelectorAll(".menu-accueil");
+  const prevButton = document.getElementById("prev-button");
+  const nextButton = document.getElementById("next-button");
+  const slidePosition = document.getElementById("slide-position");
 
-let slideIndex = 0;
-const slides = document.querySelectorAll(".menu-accueil");
-const totalSlides = slides.length;
+  let currentSlide = 0;
 
-function showSlide(index) {
-  carouselSlides.style.transform = `translateX(-${index * 100}%)`;
-  slidePosition.textContent = `${index + 1} / ${totalSlides}`;
-}
+  // Show the carousel controls (buttons and position indicator) on mobile
+  const carouselControls = document.getElementById("carousel-btn");
+  carouselControls.style.display = "block";
 
-function prevSlide() {
-  slideIndex = (slideIndex - 1 + totalSlides) % totalSlides;
-  showSlide(slideIndex);
-}
+  // Update the slide position indicator
+  function updateSlidePosition() {
+    slidePosition.textContent = `${currentSlide + 1} / ${slides.length}`;
+  }
 
-function nextSlide() {
-  slideIndex = (slideIndex + 1) % totalSlides;
-  showSlide(slideIndex);
-}
+  // Show the current slide and hide the rest
+  function showSlide() {
+    slides.forEach((slide, index) => {
+      slide.style.display = index === currentSlide ? "inline-block" : "none";
+    });
+  }
 
-prevButton.addEventListener("click", prevSlide);
-nextButton.addEventListener("click", nextSlide);
+  // Move to the next slide
+  function nextSlide() {
+    currentSlide = (currentSlide + 1) % slides.length;
+    showSlide();
+    updateSlidePosition();
+  }
 
-// Automatically change slides every 3 seconds
-function autoSlide() {
-  nextSlide();
-  setTimeout(autoSlide, 3000);
-}
+  // Move to the previous slide
+  function prevSlide() {
+    currentSlide = (currentSlide - 1 + slides.length) % slides.length;
+    showSlide();
+    updateSlidePosition();
+  }
 
-autoSlide();
+  // Attach event listeners to buttons
+  nextButton.addEventListener("click", nextSlide);
+  prevButton.addEventListener("click", prevSlide);
+
+  // Start the carousel by moving to the next slide every 3 seconds
+  setInterval(nextSlide, 3000);
+
+    // Hide all menu items
+    for (let i = 0; i < slides.length; i++) {
+      slides[i].classList.remove('active');
+    }
+
+});
