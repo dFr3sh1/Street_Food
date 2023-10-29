19d7e03146e1bb4c2d96a66745900b98f78569bf
DFr3sh <dfr3sh1@protonmail.com>
2023-10-29 17:47:08 +0100

    Updated Swiper modules and css modified for carousel style

--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
 const swiper = new Swiper('.swiper', {
-
-// Play parameters
+  // Play parameters
   slidesPerView: 1,
   spaceBetween: 20, // Adjust the gap between slides
   centeredSlides: true,
@@ -10,48 +9,59 @@ const swiper = new Swiper('.swiper', {
     enabled: true,
   },
   autoplay: {
-  delay: 3000, // Rotate every 3 seconds
-},
-//Responsive breakpoints
+    delay: 3000, // Rotate every 3 seconds
+  },
+
+  // Responsive breakpoints
   breakpoints: {
-    '@0.75': {
+    768: {
       slidesPerView: 2,
       spaceBetween: 20,
     },
-    '@1.00': {
+    1024: {
       slidesPerView: 3,
       spaceBetween: 40,
     },
-    '@1.50': {
+    1536: {
       slidesPerView: 4,
       spaceBetween: 50,
     },
-},
+  },
 
-// If we need pagination
+  // Pagination
   pagination: {
     el: '#carousel-container .swiper-pagination',
     clickable: true,
     renderBullet: function(index, className) {
       return `<div class=${className}>
-      <span class="number">${index +1}</span>
+      <span class="number">${index + 1}</span>
       <span class="line"></span>
       </div>`
     }
-},
+  },
 
-// Navigation arrows
+  // Navigation arrows
   navigation: {
-    nextEl: '#nav-rigth',
+    nextEl: '#nav-right',
     prevEl: '#nav-left',
-    },
+    clickable: true,
+  },
 
-// And if we need scrollbar
+  // Scrollbar
   scrollbar: {
     el: '.swiper-scrollbar',
-    },
+  },
 });
 
 // Show the carousel controls (buttons and position indicator) on mobile
 const carouselControls = document.getElementById('carousel-btn');
 carouselControls.style.display = 'flex';
+
+const element = document.getElementById('carousel-btn');
+
+if (element) {
+  // The element exists, so you can safely access its style property.
+  element.style.display = 'block';
+} else {
+  // The element does not exist, so you should not try to access its style property.
+}
